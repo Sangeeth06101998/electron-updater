1a3025e09a675d04178b509bd4b8f69788b65013
Sangeeth <sangeeth.ka@proglint.com>
2022-04-13 16:08:29 +0530

    Changed the text in app.js and changed the version

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Hello Welcome
+					Welcome to proglint
 				</p>
 			</header>
 		</div>
